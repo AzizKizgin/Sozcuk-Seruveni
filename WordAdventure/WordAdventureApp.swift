954ba2d721906b2580ef84f211ddf7e43efa903b
Aziz Kızgın <azizkzgn@gmail.com>
2023-11-25 17:02:11 +0300

    Initial Commit

---
+++ WordAdventure/WordAdventureApp.swift
@@ -0,0 +1,30 @@
+//
+//  WordAdventureApp.swift
+//  WordAdventure
+//
+//  Created by Aziz Kızgın on 25.11.2023.
+//
+
+import SwiftUI
+
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct WordAdventureApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
