790b5f7a5bff799af22acb090fbe7ec6e00b2a24
Aziz Kızgın <azizkzgn@gmail.com>
2023-12-02 18:21:15 +0300

    feat: screen updated

--- WordAdventure/Screens/DailyGame/DailyGameView.swift
+++ WordAdventure/Screens/DailyGame/DailyGameView.swift
@@ -8,8 +8,87 @@
 import SwiftUI
 
 struct DailyGameView: View {
+    @StateObject var dailyGameViewModel = DailyGameViewModel()
+    @Environment(\.dismiss) var dismiss
+    @FocusState var isFocused
+    @State var letterSize: CGFloat = 120
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text(dailyGameViewModel.currentQuestion?.meaning ?? "")
+                .frame(height: 250)
+            HStack{
+                TextField("Cevap", text: $dailyGameViewModel.answer)
+                    .focused($isFocused)
+                    .onSubmit {
+                        dailyGameViewModel.checkAnswer()
+                    }
+                Button(action: dailyGameViewModel.checkAnswer, label: {
+                    dailyGameViewModel.answer.isEmpty ? Text("Pas"): Text("Cevapla")
+                })
+                .buttonStyle(.borderedProminent)
+            }
+            .padding(10)
+            .background(Color.accent.opacity(0.1))
+            .cornerRadius(15)
+        }
+        .navigationBarBackButtonHidden(true)
+        .toolbar{
+            ToolbarItem(placement: .topBarLeading){
+                Button {
+                    dailyGameViewModel.showCloseAlert.toggle()
+                } label: {
+                    HStack {
+                        Image(systemName: "chevron.backward")
+                        Text("Close")
+                    }
+                }
+            }
+        }
+        .frame(maxHeight: .infinity)
+        .padding()
+        .overlay(alignment: .top){
+            VStack(spacing:10){
+                HStack{
+                    GameHeader(
+                        letter: dailyGameViewModel.currentLetter,
+                        letterSize: letterSize,
+                        answerState: dailyGameViewModel.currentQuestion?.answerState ?? .none,
+                        isFocused: isFocused,
+                        onFinish: {dailyGameViewModel.showNoQuestionAlert.toggle()}
+                    )
+                }
+            }
+        }
+        .onAppear{
+            dailyGameViewModel.getQuestions{ error in
+                if error != nil{
+                    dailyGameViewModel.showNoQuestionAlert.toggle()
+                }
+            }
+        }
+        .alert("Sorular yüklenemedi", isPresented: $dailyGameViewModel.showNoQuestionAlert){
+            Button("Tamam", role: .cancel) {
+                dailyGameViewModel.showNoQuestionAlert.toggle()
+            }
+        }
+        .alert("Çıkmak istediğinden emin misin?", isPresented: $dailyGameViewModel.showCloseAlert){
+            Button("Hayır", role: .cancel) {
+                dailyGameViewModel.showCloseAlert.toggle()
+            }
+            Button("Evet", role: .destructive) {
+                dismiss()
+            }
+        }
+        .onChange(of: isFocused){ _, newFocus in
+            withAnimation{
+                if newFocus{
+                    letterSize = 50
+                }
+                else{
+                    letterSize = 120
+                }
+            }
+        }
     }
 }
 
