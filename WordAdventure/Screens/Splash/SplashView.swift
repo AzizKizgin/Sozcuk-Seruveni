d1e8e7c9992644b362f058023c9299a4036bb891
Aziz Kızgın <azizkzgn@gmail.com>
2023-12-02 18:22:00 +0300

    feat: splashscreen added

--- WordAdventure/Screens/Splash/SplashView.swift
+++ WordAdventure/Screens/Splash/SplashView.swift
@@ -8,8 +8,22 @@
 import SwiftUI
 
 struct SplashView: View {
+    @State var iconSize: CGFloat = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+           Image("icon")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: iconSize,height: iconSize)
+              
+        }
+        .frame(maxWidth: .infinity,maxHeight: .infinity)
+        .background(.accent)
+        .onAppear{
+            withAnimation(.bouncy.speed(0.4)){
+                iconSize = 250
+            }
+        }
     }
 }
 
