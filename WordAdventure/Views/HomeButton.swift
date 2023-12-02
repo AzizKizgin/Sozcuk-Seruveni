c59564dbc37fe5a389eeb892a1b7e798bb63e5db
Aziz Kızgın <azizkzgn@gmail.com>
2023-12-02 18:16:21 +0300

    feat: HomButton added

--- WordAdventure/Views/HomeButton.swift
+++ WordAdventure/Views/HomeButton.swift
@@ -8,11 +8,38 @@
 import SwiftUI
 
 struct HomeButton: View {
+    let title: String
+    let onPress: () -> Void
+    var icon: String = "\(Utils.getDay()).circle"
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: onPress, label: {
+            HStack{
+                Text(title)
+                Spacer()
+                Image(systemName: icon)
+                    .font(.title)
+            }
+            .padding(12)
+            .overlay{
+                RoundedRectangle(cornerSize: CGSize(width: 20, height: 20))
+                    .stroke(style: .init(lineWidth: 3))
+                    .foregroundStyle(.accent)
+            }
+        })
+        .font(.title2)
+        .foregroundStyle(.accent)
+        .buttonStyle(HomeButtonStyle())
+    }
+}
+
+struct HomeButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .opacity(configuration.isPressed ? 0.7 : 1)
+            .contentShape(Rectangle())
     }
 }
 
 #Preview {
-    HomeButton()
+    HomeButton(title: "Günlük", onPress: {})
 }
