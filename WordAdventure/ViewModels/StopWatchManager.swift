ba9f475f44b9d6751589bdfefb5b99f0fa9c85f2
Aziz Kızgın <azizkzgn@gmail.com>
2023-12-03 18:07:06 +0300

    feat: StopWatchManager created

--- WordAdventure/ViewModels/StopWatchManager.swift
+++ WordAdventure/ViewModels/StopWatchManager.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+class StopWatchManager: ObservableObject {
+    
+    @Published var remainingTime = 300
+    var timer = Timer()
+    
+    func start() {
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+            self.remainingTime -= 1
+        }
+    }
+    
+    func stop() {
+        timer.invalidate()
+    }
+    
+    func formatElapsedTime() -> String {
+        let minutes = Int(self.remainingTime) / 60
+        let seconds = Int(self.remainingTime) % 60
+
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
+}
