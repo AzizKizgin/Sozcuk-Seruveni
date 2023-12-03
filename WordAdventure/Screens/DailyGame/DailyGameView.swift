d5b9677abcf04f39a57323eb1b3a94cd3abdcda2
Aziz Kızgın <azizkzgn@gmail.com>
2023-12-03 18:09:23 +0300

    feat: screen covered with vstack

--- WordAdventure/Screens/DailyGame/DailyGameView.swift
+++ WordAdventure/Screens/DailyGame/DailyGameView.swift
@@ -9,91 +9,107 @@ import SwiftUI
 
 struct DailyGameView: View {
     @StateObject var dailyGameViewModel = DailyGameViewModel()
+    @StateObject var stopWatchManager = StopWatchManager()
     @Environment(\.dismiss) var dismiss
     @FocusState var isFocused
-    @State var letterSize: CGFloat = 120
     var body: some View {
         VStack{
-            Text(dailyGameViewModel.currentQuestion?.meaning ?? "")
-                .font(UIDevice.current.userInterfaceIdiom == .phone ? .title2: .title)
-                .frame(height: 250)
-            HStack{
-                TextField("Cevap", text: $dailyGameViewModel.answer)
-                    .focused($isFocused)
-                    .onSubmit {
-                        dailyGameViewModel.checkAnswer()
+            if dailyGameViewModel.screen == .game {
+                VStack{
+                    Text(dailyGameViewModel.currentQuestion?.meaning ?? "")
+                        .font(UIDevice.current.userInterfaceIdiom == .phone ? .title2: .title)
+                        .frame(height: 250)
+                    HStack{
+                        TextField("Cevap", text: $dailyGameViewModel.answer)
+                            .focused($isFocused)
+                            .onSubmit {
+                                dailyGameViewModel.checkAnswer()
+                            }
+                            .autocorrectionDisabled()
+                            .textInputAutocapitalization(.never)
+                        Button(action: dailyGameViewModel.checkAnswer, label: {
+                            dailyGameViewModel.answer.isEmpty ? Text("Pas"): Text("Cevapla")
+                        })
+                        .buttonStyle(.borderedProminent)
                     }
-                Button(action: dailyGameViewModel.checkAnswer, label: {
-                    dailyGameViewModel.answer.isEmpty ? Text("Pas"): Text("Cevapla")
-                })
-                .buttonStyle(.borderedProminent)
-            }
-            .padding(10)
-            .background(Color.accent.opacity(0.1))
-            .cornerRadius(15)
-        }
-        .navigationBarBackButtonHidden(true)
-        .toolbar{
-            ToolbarItem(placement: .topBarLeading){
-                Button {
-                    dailyGameViewModel.showCloseAlert.toggle()
-                } label: {
-                    HStack {
-                        Image(systemName: "chevron.backward")
-                        Text("Close")
+                    .padding(10)
+                    .background(Color.accent.opacity(0.1))
+                    .cornerRadius(15)
+                }
+                .navigationBarBackButtonHidden(true)
+                .toolbar{
+                    ToolbarItem(placement: .topBarLeading){
+                        Button {
+                            dailyGameViewModel.showCloseAlert.toggle()
+                        } label: {
+                            HStack {
+                                Image(systemName: "chevron.backward")
+                                Text("Close")
+                            }
+                        }
+                    }
+                }
+                .frame(maxHeight: .infinity)
+                .padding()
+                .overlay(alignment: .top){
+                    VStack(spacing:10){
+                        HStack{
+                            GameHeader(
+                                letter: dailyGameViewModel.currentLetter,
+                                answerState: dailyGameViewModel.currentQuestion?.answerState ?? .none,
+                                remainingTime: stopWatchManager.formatElapsedTime(),
+                                isFocused: isFocused
+                            )
+                            .onAppear{
+                                stopWatchManager.start()
+                            }
+                            .onDisappear{
+                                stopWatchManager.stop()
+                            }
+                            .onChange(of: stopWatchManager.remainingTime){ oldValue, newValue in
+                                if newValue == 0 {
+                                    stopWatchManager.stop()
+                                    dailyGameViewModel.showResultScreen(time: stopWatchManager.formatElapsedTime())
+                                }
+                            }
+                        }
+                    }
+                }
+                .onAppear{
+        //            dailyGameViewModel.getQuestions{ error in
+        //                if error != nil{
+        //                    dailyGameViewModel.showNoQuestionAlert.toggle()
+        //                }
+        //            }
+                    dailyGameViewModel.questions = fakeData
+                }
+                .alert("Sorular yüklenemedi", isPresented: $dailyGameViewModel.showNoQuestionAlert){
+                    Button("Tamam", role: .cancel) {
+                        dailyGameViewModel.showNoQuestionAlert.toggle()
+                    }
+                }
+                .alert("Çıkmak istediğinden emin misin?", isPresented: $dailyGameViewModel.showCloseAlert){
+                    Button("Hayır", role: .cancel) {
+                        dailyGameViewModel.showCloseAlert.toggle()
+                    }
+                    Button("Evet", role: .destructive) {
+                        dismiss()
                     }
                 }
             }
-        }
-        .frame(maxHeight: .infinity)
-        .padding()
-        .overlay(alignment: .top){
-            VStack(spacing:10){
-                HStack{
-                    GameHeader(
-                        letter: dailyGameViewModel.currentLetter,
-                        letterSize: letterSize,
-                        answerState: dailyGameViewModel.currentQuestion?.answerState ?? .none,
-                        isFocused: isFocused,
-                        onFinish: {dailyGameViewModel.showNoQuestionAlert.toggle()}
-                    )
-                }
+            else if dailyGameViewModel.screen == .info{
+                GameInfo(onClose: dailyGameViewModel.closeInfoScreen)
+            }
+            else{
+                GameSummaryView(questions: dailyGameViewModel.questions, remainingTime: stopWatchManager.formatElapsedTime())
             }
         }
-        .onAppear{
-//            dailyGameViewModel.getQuestions{ error in
-//                if error != nil{
-//                    dailyGameViewModel.showNoQuestionAlert.toggle()
-//                }
-//            }
-            dailyGameViewModel.questions = fakeData
-        }
-        .alert("Sorular yüklenemedi", isPresented: $dailyGameViewModel.showNoQuestionAlert){
-            Button("Tamam", role: .cancel) {
-                dailyGameViewModel.showNoQuestionAlert.toggle()
-            }
-        }
-        .alert("Çıkmak istediğinden emin misin?", isPresented: $dailyGameViewModel.showCloseAlert){
-            Button("Hayır", role: .cancel) {
-                dailyGameViewModel.showCloseAlert.toggle()
-            }
-            Button("Evet", role: .destructive) {
-                dismiss()
-            }
-        }
-        .onChange(of: isFocused){ _, newFocus in
-            withAnimation{
-                if newFocus{
-                    letterSize = 50
-                }
-                else{
-                    letterSize = 120
-                }
-            }
-        }
+        .animation(.default, value: dailyGameViewModel.screen)
     }
 }
 
 #Preview {
-    DailyGameView()
+    NavigationStack{
+        DailyGameView()
+    }
 }
