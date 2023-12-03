19352fc50594ab83340c91109ee829f32c375fe3
Aziz Kızgın <azizkzgn@gmail.com>
2023-12-03 18:07:53 +0300

    feat: Gameinfo created

--- WordAdventure/Screens/GameInfo/GameInfo.swift
+++ WordAdventure/Screens/GameInfo/GameInfo.swift
@@ -6,13 +6,42 @@
 //
 
 import SwiftUI
-
 struct GameInfo: View {
+    let onClose: () -> Void
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView{
+            VStack(spacing: 40){
+                Text("Nasıl Oynanır?")
+                    .bold()
+                Text("""
+                    Sözcük Serüveni, 27 harf, her harf için bir kelime ve bu kelimenin anlamını içerir.
+
+                    Örneğin \(Text("A").bold()) harfi için sorulabilecek \(Text("\"\("Ağaçtan, tahtadan yapılmış")\"").bold()) sorusunun cevabı \(Text("\"\("Ahşap")\"").bold()) olacaktır.
+
+                    Toplam süre \(Text("\"\("5 dakikadır")\"").bold()).
+
+                    Bu süre içinde belirtilen alana cevabını yazmalı ya da \(Text("\"\("Pas")\"").bold()) butonuna basmalısın.
+
+                    Süre bittiğinde oyun biter ve seni sonuç sayfasına yönlendirir. Oyunu erkenden bitirmek için \(Text("\"\("Bitir")\"").bold()) yazabilirsin.
+                    """)
+                HStack{
+                    Button(action: onClose, label: {
+                        Text("Kapat ve Başla")
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 30)
+                            .foregroundStyle(.white)
+                    })
+                    .buttonStyle(.borderedProminent)
+                    .frame(maxWidth: .infinity)
+                }
+            }
+            .padding()
+            .font(UIDevice.current.userInterfaceIdiom == .phone ? .title3: .title)
+            .foregroundStyle(.accent)
+        }
     }
 }
 
 #Preview {
-    GameInfo()
+    GameInfo(onClose: {})
 }
