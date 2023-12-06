e89a1e2133e97c45cc55e749a3048d20dabf149a
Aziz Kızgın <azizkzgn@gmail.com>
2023-12-06 21:20:59 +0300

    feat: localizedLowercased and localizedUppercased added

--- WordAdventure/Resources/String.swift
+++ WordAdventure/Resources/String.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+extension String{
+    func localizedLowercased() -> String{
+        return self.lowercased(with: .init(identifier: "tr_TR"))
+    }
+    
+    func localizedUppercased() -> String{
+        return self.uppercased(with: .init(identifier: "tr_TR"))
+    }
+}
