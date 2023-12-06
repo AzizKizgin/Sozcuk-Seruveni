f6bd1e1dad9c9621303344706950416d56c5003b
Aziz Kızgın <azizkzgn@gmail.com>
2023-12-06 21:25:40 +0300

    feat: gamesummaryview updated

--- WordAdventure/Screens/GameSummary/GameSummaryView.swift
+++ WordAdventure/Screens/GameSummary/GameSummaryView.swift
@@ -6,27 +6,36 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct GameSummaryView: View {
+    @Environment(\.modelContext) private var modelContext
     @State private var tabSelection = 0
+    @Query var savedDailyGame: [DailyGame]
     let questions: [Question]
     let remainingTime: String
+    var saveQuestions: Bool = false
     var body: some View {
         TabView(selection: $tabSelection){
             ScoreView(
                 correctCount: getQuestionsStats().correctCount,
                 wrongCount: getQuestionsStats().wrongCount,
                 passCount: getQuestionsStats().passCount,
-                remainingTime: remainingTime
+                remainingTime: getRemainingTime()
             )
                 .tag(0)
-            AnswersView(questions: questions)
+            AnswersView(questions: getQuestions())
                 .tag(1)
         }
         .tabViewStyle(DefaultTabViewStyle())
         .overlay(alignment: .bottom){
             BottomBarView(tabSelection: $tabSelection)
                 .padding(.bottom,4)
+        }
+        .onAppear{
+            if saveQuestions {
+                saveDailyGame()
+            }
         }
     }
 }
@@ -36,26 +45,61 @@ struct GetQuestionsStatsType{
     let wrongCount:Int
     let passCount:Int
 }
+
 extension GameSummaryView{
+    func getRemainingTime() -> String{
+        let time: String = savedDailyGame.isEmpty
+            ? remainingTime
+            : savedDailyGame[0].remainingTime
+        return time
+    }
+    
+    func getQuestions() -> [Question]{
+        let questionArray: [Question] = savedDailyGame.isEmpty || savedDailyGame[0].dailyGameQuestions.isEmpty
+            ? questions
+            : savedDailyGame[0].dailyGameQuestions
+        return questionArray
+    }
+    
     func getQuestionsStats() -> GetQuestionsStatsType{
         var correctCount = 0
         var wrongCount = 0
         var passCount = 0
-        
-        questions.forEach({
+        let questionArray: [Question] = getQuestions()
+        questionArray.forEach({
             switch $0.answerState {
-            case .isCorrect:
+            case AnswerState.isCorrect:
                 correctCount += 1
-            case .isWrong:
+            case AnswerState.isWrong:
                 wrongCount += 1
-            case .isPassed:
+            case AnswerState.isPassed:
                 passCount += 1
-            case .none:
+            case AnswerState.none:
+                passCount += 1
+            default:
                 passCount += 1
             }
         })
         
         return GetQuestionsStatsType(correctCount: correctCount, wrongCount: wrongCount, passCount: passCount)
+    }
+    
+    func saveDailyGame(){
+        do{
+            if savedDailyGame.isEmpty{
+                let newDailyGame = DailyGame(date: .now, dailyGameQuestions: questions, remainingTime: remainingTime)
+                modelContext.insert(newDailyGame)
+            }
+            else if let savedData = savedDailyGame.first, !Utils.isDateToday(savedData.date){
+                modelContext.delete(savedData)
+                let newDailyGame = DailyGame(date: .now, dailyGameQuestions: questions, remainingTime: remainingTime)
+                modelContext.insert(newDailyGame)
+            }
+            try modelContext.save()
+        }
+        catch{
+            
+        }
     }
 }
 
